bad9fe4a5ce6b298e109a9fb8e075726f83d1007
ABParisiAlessandro <ab.alessandro.parisi@gmail.com>
2022-12-03 10:42:23 +0100

    Initial commit

---
+++ src/Esercizio1/Potenza.java
@@ -0,0 +1,13 @@
+package Esercizio1;
+
+public class Potenza {
+    int number;
+    int base;
+    public Potenza(int number){
+        this.number=number;
+        base =2;
+    }
+    public void pow(){
+        System.out.println((int)Math.pow(base,number));
+    }
+}
