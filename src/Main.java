0de00ea32ba99b48949a7a9096715a88fb434040
ABParisiAlessandro <ab.alessandro.parisi@gmail.com>
2022-12-05 17:18:15 +0100

    We Carlo!!

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        int a = 120;
-        int b = 121;
-        int c = 137;
 
-        System.out.println(a/60+"\t"+b/60+"\t"+c/60);
     }
 }
